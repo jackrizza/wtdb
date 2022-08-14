a7b34afb2aec55b24a13fe92f2f27e4fdef64c0b
Jack Rizza <jackrizza@gmail.com>
2022-08-13 23:18:52 -0400

    Switched to w3schools query examples

--- examples/tests.rs
+++ examples/tests.rs
@@ -5,19 +5,17 @@ use wtdb::connection::Conn;
 
 fn main() {
     
-    let mut db : Conn = Conn::new("127.0.0.1", "6942", "augustus", "augustus"); 
+    let mut db : Conn = Conn::new("127.0.0.1", "6942", "jack", "jack"); 
 
-    let query_string : String = "SELECT * FROM testdb".into();
+    let query_string : String = "SELECT * FROM Customers;".into();
     let _ = db.execute(query_string);
 
-    let query_string : String = "SELECT id, name FROM testdb".into();
-    let _ = db.execute(query_string);
-
-
-    let query_string : String = "UPDATE INTO testdb (id, name) VALUES (1, 'jack')".into();
+    let query_string : String = "INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country)VALUES ('Cardinal','Tom B. Erichsen','Skagen 21','Stavanger','4006','Norway');".into();
     let _ = db.execute(query_string);
     
 
+    let query_string : String = "UPDATE Customers SET ContactName='Alfred Schmidt', City='Frankfurt' WHERE CustomerID=1;".into();
+    let _ = db.execute(query_string);
 
     db.evt_log.log_print();
 }
